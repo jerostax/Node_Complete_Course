fdf04d8dd4d54f2158181c540de05aeaea840530
jerostax <jeremy.geneste@gmail.com>
2019-11-15 17:43:54 +0100

    route handler/middleware

--- expressjs/app.js
+++ expressjs/app.js
@@ -9,11 +9,19 @@ const app = express();
 // elle a 3 args => request object, response object, next
 // next est une function qui doit s'éxécuter pour autoriser une requête à aller au prochain middleware
 
+/*
+
 app.use((req, res, next) => {
   console.log("In the middleware!");
   next(); // autorise la requête à passer dans le prochain middleware
 });
-app.use((req, res, next) => {
+
+*/
+app.use("/users", (req, res, next) => {
+  res.send("<h1>User's page</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware!");
   // send() nous autorise à renvoyer une réponse (avec un body de type any)
   res.send("<h1>Hello from Express</h1>");
