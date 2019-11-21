0ca7aa8e3c02547baeb88c72bc8ff501b6da4aff
jerostax <jeremy.geneste@gmail.com>
2019-11-21 10:16:42 +0100

    refacto Product model with helper functions

--- expressjs/models/product.js
+++ expressjs/models/product.js
@@ -2,6 +2,28 @@
 const fs = require('fs');
 const path = require('path');
 
+// Helper function pour le path
+// Ici on veut sauvegarder nos produits dans un fichier .json à l'interieur du dossier data au niveau de notre app (stocké dans notre variable p)
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'product.json'
+);
+
+// Helper function
+const getProductsFromFile = callback => {
+  // On veut lire le fichier JSON
+  fs.readFile(p, (err, fileContent) => {
+    // S'il y a rien on veut quand même retourner quelque chose (un array vide ici)
+    if (err) {
+      callback([]);
+    } else {
+      // Sinon on retourne notre contenu parsé (sous forme de tableau js)
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
@@ -9,25 +31,11 @@ module.exports = class Product {
 
   // Méthode pour sauvegarder les produits
   save() {
-    // Ici on veut sauvegarder nos produits dans un fichier .json à l'interieur du dossier data au niveau de notre app (stocké dans notre variable p)
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'product.json'
-    );
-    // Ici on dit qu'on veut lire le fichier qui se trouve dans le path "p" (voir const jsute au dessus)
-    // On aura ou une erreur ou le contenu du fichier
-    fs.readFile(p, (err, fileContent) => {
-      console.log(fileContent);
-      let products = [];
-      // S'il n'y a pas d'errur alors on veut lire le produit depuis le fichier JSON
-      // On doit donc parse le fichier pour qu'il transforme le json en un tableau/obj javascript
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    // On récupère les produits qui sont dans le fichier JSON
+    getProductsFromFile(products => {
       // On ajoute le nouveau produit au tableau products
       products.push(this);
-      // Ici on va enregistre le produit dans le fichier JSON
+      // Ici on va enregistre/écrit le produit dans le fichier JSON
       // On doit donc transformer l'array/obj... javascript en JSON grâce à stringify
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -36,19 +44,6 @@ module.exports = class Product {
   }
   // Méthode pour "charger" nos produits
   static fetchAll(callback) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'product.json'
-    );
-    // On veut lire le fichier JSON
-    fs.readFile(p, (err, fileContent) => {
-      // S'il y a rien on veut quand même retourner quelque chose (un array vide ici)
-      if (err) {
-        callback([]);
-      }
-      // Sinon on retourne notre contenu parsé (sous forme de tableau js)
-      callback(JSON.parse(fileContent));
-    });
+    getProductsFromFile(callback);
   }
 };
