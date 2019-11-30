efba26aab7cbabca6f4afd38b7d3177fa301e053
Jérémy Geneste <45629705+jerostax@users.noreply.github.com>
2019-11-30 19:32:08 +0100

    Update path.js

--- expressjs/util/path.js
+++ expressjs/util/path.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 // On utilise la variable global process pour spécifier qu'on veut passer le path au 'main module' de l'app,
-// En gros app.js
+// En gros la racine du projet 
 module.exports = path.dirname(process.mainModule.filename);
