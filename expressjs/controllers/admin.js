8e74beb1a8d33052c59db327aebb4941956287a7
Jeremy <jeremy.geneste@gmail.com>
2019-12-11 11:20:04 +0100

    find products and edit products related to the user

--- expressjs/controllers/admin.js
+++ expressjs/controllers/admin.js
@@ -66,8 +66,12 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  // Ici on cherche les produits qui ont relié a l'user (retourne un tableau)
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -134,7 +138,10 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Ici on cherche les produits rattachés à l'user
+  req.user
+    .getProducts()
+    // Product.findAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
