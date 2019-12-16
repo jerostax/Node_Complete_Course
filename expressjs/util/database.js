3f0be77badb83b26bd5e44f18349ddcbd301c8ab
Jeremy <jeremy.geneste@gmail.com>
2019-12-16 11:37:41 +0100

    connection to mongodb db

--- expressjs/util/database.js
+++ expressjs/util/database.js
@@ -1,31 +1,35 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const mongoURI = process.env.MONGODB_URI;
+const mongodb = require('mongodb');
+// MongoClient = MongoDB constructor
+const MongoClient = mongodb.MongoClient;
 
-// On instancie un nouveau constructor sequelize qui prend plusieurs options
-// 1 : database name
-// 2 : username
-// 3 : password
-// 4: un objet dans lequel on précise le language (ici mysql) et le host (il y a encore d'autres options qu'on pourrait préciser dans cet objet)
-const sequelize = new Sequelize('node-complete', 'root', process.env.DB_PASS, {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  // On se connect à mongoDB (prend l'url de connect your application dans le cluster mongoDB que j'ai stocké danas le .env)
+  // La connexion retourne une promesse
+  MongoClient.connect(mongoURI)
+    .then(client => {
+      console.log('Connect to MongoDB !');
+      // On enregistre la connexion dans la variable _db
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
 
-// ***** ANCIEN CODE CONNEXION SQL BDD SANS SEQUELIZE
-// **
-// **
-// const mysql = require('mysql2');
-// require('dotenv').config();
+const getDb = () => {
+  // On check si la connexion s'est bien enregistrée dans _db et on la retourne
 
-// // Icion créé une 'pool' de connexions qui va nous permettre de toujours avoir accès à la db
-// // à chaque fois qu'on a une query afin d'avoir une connexion à chaque fois
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   database: 'node-complete',
-//   password: process.env.DB_PASS
-// });
+  if (_db) {
+    return _db;
+  }
+  throw 'No databse found!';
+};
 
-// module.exports = pool.promise();
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
