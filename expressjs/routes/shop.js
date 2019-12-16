aba56e389f4a4a117aa71fb6aa9866eda3197d29
Jeremy <jeremy.geneste@gmail.com>
2019-12-16 12:05:07 +0100

    commented shop routes to slowly refacto with mongodb

--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -16,20 +16,20 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId', shopController.getProduct);
+// router.get('/products/:productId', shopController.getProduct);
 
-// GET card
-router.get('/cart', shopController.getCart);
-// POST card
-router.post('/cart', shopController.postCart);
+// // GET card
+// router.get('/cart', shopController.getCart);
+// // POST card
+// router.post('/cart', shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.post('/create-order', shopController.postOrder);
+// router.post('/create-order', shopController.postOrder);
 
-router.get('/orders', shopController.getOrders);
+// router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout);
+// router.get('/checkout', shopController.getCheckout);
 
 // ***** SANS LE MVC PATTERN *****
 // *
