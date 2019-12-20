0c5d3b4cb9a427962d40b6f4055b6cda31ff0af4
Jeremy <jeremy.geneste@gmail.com>
2019-12-20 10:32:30 +0100

    use the express session middleware

--- expressjs/controllers/auth.js
+++ expressjs/controllers/auth.js
@@ -1,14 +1,17 @@
 exports.getLogin = (req, res, next) => {
   // Ici on récupère la valeur true ou false de isLoggedIn dans le Cookie
-  const isLoggedIn = req.get('Cookie').split('=')[1];
+  // const isLoggedIn = req.get('Cookie').split('=')[1];
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
+  // Ancien code pour set un cookie sans session
+  // res.setHeader('Set-Cookie', 'loggedIn=true');
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
