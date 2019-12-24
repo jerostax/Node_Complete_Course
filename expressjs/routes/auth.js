5acb0661d81c52557eec0e9c2843292ed34ad74c
Jeremy <jeremy.geneste@gmail.com>
2019-12-24 11:13:21 +0100

    added reset password route + controller + view

--- expressjs/routes/auth.js
+++ expressjs/routes/auth.js
@@ -13,4 +13,6 @@ router.post('/logout', authController.postLogout);
 
 router.post('/signup', authController.postSignup);
 
+router.get('/reset', authController.getReset);
+
 module.exports = router;
