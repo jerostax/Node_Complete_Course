47330ca739a9a85e5a0e18cebc780ee3039f8caf
Jeremy <jeremy.geneste@gmail.com>
2020-01-08 10:57:20 +0100

    sending and handling background requests to delete a product

--- expressjs/public/js/admin.js
+++ expressjs/public/js/admin.js
@@ -2,5 +2,21 @@ const deleteProduct = btn => {
   console.log(btn);
   // Ici on accède à l'input qui a le name productId pour récupérer cet id
   const prodId = btn.parentNode.querySelector('[name=productId]').value;
-  const csrf = btn.parentNode.querySelector('[name=_crsf]').value;
+  // Même opération avec le csrf token
+  const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+  // fetch() est une méthode comprise par le navigateur pour envoyer ou fetch des requêtes http
+  fetch('/admin/product/' + prodId, {
+    // Ici on set la méthode et le csrf token dans le headers
+    method: 'DELETE',
+    headers: {
+      'csrf-token': csrf
+    }
+  })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
