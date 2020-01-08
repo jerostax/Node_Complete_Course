0c72ec80bd6242cbd520297256d9e7d768f147b2
Jeremy <jeremy.geneste@gmail.com>
2020-01-08 11:03:52 +0100

    removing product article from the DOM on delete

--- expressjs/public/js/admin.js
+++ expressjs/public/js/admin.js
@@ -5,6 +5,10 @@ const deleteProduct = btn => {
   // Même opération avec le csrf token
   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+  // closest() est une méthode qui nous permet de trouver l'élement le plus proche
+  // ici l'élement avec la classe article le plus proche du btn sur lequel on a cliqué (ca va nous permettre de delete l'article côté front)
+  const productElement = btn.closest('article');
+
   // fetch() est une méthode comprise par le navigateur pour envoyer ou fetch des requêtes http
   fetch('/admin/product/' + prodId, {
     // Ici on set la méthode et le csrf token dans le headers
@@ -14,7 +18,14 @@ const deleteProduct = btn => {
     }
   })
     .then(result => {
-      console.log(result);
+      // console.log(result);
+      return result.json();
+    })
+    .then(data => {
+      console.log(data);
+      // Ici on remove/supprimé l'élément article qui contient le produit qu'on vient de delete
+      // note: pourrait être productElement.remove() mais c'est pas compris par IE donc on utilise le code ci-dessous
+      productElement.parentNode.removeChild(productElement);
     })
     .catch(err => {
       console.log(err);
