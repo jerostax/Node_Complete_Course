221b65d44983ea88f88d724a8a3d258683c816ca
Jeremy <jeremy.geneste@gmail.com>
2020-01-09 10:04:20 +0100

    store created post on database

--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -20,25 +22,30 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: 'Validation failed, entered data is incorrect.',
-        errors: errors.array()
-      });
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array()
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  // 201 = "success a ressource is created"
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      _id: new Date().getTime(),
-      title: title,
-      content: content,
-      creator: { name: 'Jérémy' },
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/chamonix.jpg',
+    creator: { name: 'Jérémy' }
   });
+  post
+    .save()
+    .then(result => {
+      console.log(result);
+      // 201 = "success a ressource is created"
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
