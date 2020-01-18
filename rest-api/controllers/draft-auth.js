e908eec051d1a44094405034125c5654df6d02f9
Jeremy <jeremy.geneste@gmail.com>
2020-01-18 12:10:47 +0100

    added comment on top of draft-auth

--- rest-api/controllers/draft-auth.js
+++ rest-api/controllers/draft-auth.js
@@ -1,3 +1,8 @@
+// *
+// *
+// * * * THIS CODE IS WITHOUT ASYNC AWAIT SYNTAXE  * * *
+// *
+// *
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
