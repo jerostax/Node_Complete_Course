84ad3af1c0c5c0146c89914adec20af7e34e3519
Jeremy <jeremy.geneste@gmail.com>
2020-01-19 12:22:23 +0100

    using socket in feed.js to establish a channel post and send new post to all connected users

--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const { validationResult } = require('express-validator/check');
 
+const io = require('./socket');
 const Post = require('../models/post');
 const User = require('../models/user');
 
@@ -57,6 +58,10 @@ exports.createPost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.push(post);
     await user.save();
+    // On récupère notre objet IO grâce à la fonction getIO() qui retourne io dans socket.js
+    // avec emit() on créé un channel posts, on décrit une action et on renvoi le contenu, ici le nouveau post
+    // Ca nous permet d'envoyer à tous les clients connectés
+    io.getIO().emit('posts', { action: 'create', post: post });
     res.status(201).json({
       message: 'Post created successfully!',
       post: post,
