295a267462a1c5d7ff14b007818af51fbd939b68
jeremygen <jeremy.geneste@edifixio.com>
2020-07-03 15:24:21 +0200

    added createPost mutation & resolver

--- graphql-api/graphql/resolvers.js
+++ graphql-api/graphql/resolvers.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const validator = require('validator');
 
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = {
   // on peut récupérer l'email de cette façon et il faut return le User.findOne() si on utilise then()
@@ -73,5 +74,43 @@ module.exports = {
     );
 
     return { token: token, userId: user._id.toString() };
+  },
+
+  createPost: async function ({ postInput }, req) {
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title is invalid' });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is invalid' });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+    });
+
+    const createdPost = await post.save();
+
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString(),
+    };
   },
 };
