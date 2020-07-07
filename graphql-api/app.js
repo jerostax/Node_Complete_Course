9afe90f3d1a513b098ecd019e3bc61952fc472d7
jeremygen <jeremy.geneste@gmail.com>
2020-07-07 16:33:08 +0200

    image uploads

--- graphql-api/app.js
+++ graphql-api/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -58,6 +59,22 @@ app.use((req, res, next) => {
 // AUTH MIDDLEWARE
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  console.log('HERE ?');
+  return res
+    .status(201)
+    .json({ message: 'File stored', filePath: req.file.path });
+});
+
 app.use(
   '/graphql',
   graphqlHttp({
@@ -90,3 +107,8 @@ mongoose
     const server = app.listen(8080);
   })
   .catch((err) => console.log(err));
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
