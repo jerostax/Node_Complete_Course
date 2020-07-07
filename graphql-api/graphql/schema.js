6193bdaeb214f86c02020c91df0a0c6fa8bd5f58
jeremygen <jeremy.geneste@gmail.com>
2020-07-07 15:43:41 +0200

    create and get posts query in front end + minor fix in login RootQuery

--- graphql-api/graphql/schema.js
+++ graphql-api/graphql/schema.js
@@ -44,7 +44,7 @@ module.exports = buildSchema(`
     }
 
     type RootQuery {
-        login(email: String!, password: String!): AuthData
+        login(email: String!, password: String!): AuthData!
         posts: PostData!
     }
 
